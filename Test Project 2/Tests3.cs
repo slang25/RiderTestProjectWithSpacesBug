560c0617bd25c0461a4b55e839d6ed17275c8d96
Stuart Lang <stuart.b.lang@gmail.com>
2025-09-09 10:16:57 +0100

    Initial commit :tada:

---
+++ Test Project 2/Tests3.cs
@@ -0,0 +1,14 @@
+﻿namespace Test_Project_2;
+
+[ClassDataSource<DataClass>]
+[ClassConstructor<DependencyInjectionClassConstructor>]
+public class AndEvenMoreTests(DataClass dataClass)
+{
+    [Test]
+    public void HaveFun()
+    {
+        Console.WriteLine(dataClass);
+        Console.WriteLine("For more information, check out the documentation");
+        Console.WriteLine("https://tunit.dev/");
+    }
+}
